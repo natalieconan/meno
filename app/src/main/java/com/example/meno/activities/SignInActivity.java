80cd1ecd20e7f88ab76ccce2242ee347eb1c0ac3
natalieconan <natalieconan17@gmail.com>
2024-03-23 18:07:20 +0700

    create signup, login logic class

--- app/src/main/java/com/example/meno/activities/SignInActivity.java
+++ app/src/main/java/com/example/meno/activities/SignInActivity.java
@@ -1,2 +1,28 @@
-package com.example.meno.activities;public class SignInActivity {
+package com.example.meno.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.meno.databinding.ActivitySignInBinding;
+
+public class SignInActivity extends AppCompatActivity {
+    private ActivitySignInBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivitySignInBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setListeners();
+    }
+
+    protected void setListeners() {
+        binding.textCreateNewAccount.setOnClickListener(v ->
+                startActivity(new Intent(
+                        getApplicationContext(),
+                        SignUpActivity.class
+                )));
+    }
 }
--- app/src/main/java/com/example/meno/activities/SignUpActivity.java
+++ app/src/main/java/com/example/meno/activities/SignUpActivity.java
@@ -1,2 +1,24 @@
-package com.example.meno.activities;public class SignUpActivity {
+package com.example.meno.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.meno.databinding.ActivitySignUpBinding;
+
+public class SignUpActivity extends AppCompatActivity {
+    private ActivitySignUpBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setListeners();
+    }
+
+    protected void setListeners() {
+        binding.textSignIn.setOnClickListener(v -> onBackPressed());
+    }
 }
