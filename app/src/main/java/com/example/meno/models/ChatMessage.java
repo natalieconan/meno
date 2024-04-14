2b2a1057bbf7784a9a7d94460bd0531efe52cc3d
Entropt <entropt05@gmail.com>
2024-04-14 15:33:03 +0700

    update chat model

--- app/src/main/java/com/example/meno/models/ChatMessage.java
+++ app/src/main/java/com/example/meno/models/ChatMessage.java
@@ -3,6 +3,7 @@ package com.example.meno.models;
 import java.util.Date;
 
 public class ChatMessage {
-    public String senderId, receiverId, message, dateTime;
+    public String senderId, receiverId, message, dateTime, type;
     public Date dateObject;
+    public String conversationId, conversationName, conversationImage;
 }
